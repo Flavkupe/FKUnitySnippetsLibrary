8ce19ae0c0a718af361e8589c24689e906a3e914
Flavio <flavkupe@gmail.com>
2025-01-20 13:09:08 -0800

    Fixed thing

--- FKUnitySnippetsLibrary/Coroutines/Timer/TimerDemo.cs
+++ FKUnitySnippetsLibrary/Coroutines/Timer/TimerDemo.cs
@@ -68,5 +68,10 @@ namespace FKUnitySnippets.Coroutines
                 StopSpawningObjects();
             }
         }
+
+        void OnDestroy()
+        {
+            StopSpawningObjects();
+        }
     }
 }
